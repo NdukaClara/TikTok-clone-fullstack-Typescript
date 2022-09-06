0e00ac7cecc6ceb5dcb55aab2180b593d24e55a1
Nduka Clara <93293058+NdukaClara@users.noreply.github.com>
2022-09-06 14:16:39 +0100

    first commit

---
+++ components/NoResults.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface IProps {
+  text: string;
+}
+
+const Noresults = ({ text }: IProps) => {
+  return <div>Noresults</div>;
+};
+
+export default Noresults;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  // swcMinify: true,
+  images: {
+    domains: ["i.postimg.cc", "lh3.googleusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
